440017044302639c6131f9457af515cf785cc555
Anastassia Kuznetsova <54169555+honeynastu@users.noreply.github.com>
2020-11-01 01:13:05 +0300

    Delete utils.ts

--- src/utils.ts
+++
@@ -1,8 +0,0 @@
-import { ethereum } from "@graphprotocol/graph-ts";
-
-/**
- * If nothing better is available, this generates a unique id from the transaction hash + log index.
- */
-export function getIDFromEvent(event: ethereum.Event): string {
-  return event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-}
